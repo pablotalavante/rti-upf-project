069f96c6535ad5f1f16152efbb1fa97be742196c
pablotalavante <pablot94@gmail.com>
2021-03-01 16:37:35 +0100

    initial commit

---
+++ RTI_Class/Assets/Scripts/Trigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{	
+
+	public GameObject env;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+    	if(other.tag=="Player"){
+    		Vector3 deltaY = new Vector3(0, -100, 0);
+    		env.transform.Translate(deltaY);
+    	}
+    	Debug.Log(other.tag);
+    	Debug.Log("okey");
+    }
+}
---
+++ RTI_Class/Assets/Scripts/control.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+	public float speed;
+	public float speedR;
+	float walkSpeed;
+	float runSpeed;
+
+	// Use this for initialization
+	void Awake()
+	{
+
+	}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+			GetComponent<CharacterController>().Move(transform.forward * speed);
+		}
+		if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) { 
+           GetComponent<CharacterController>().Move(transform.forward* -1 * speed);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) { 
+            transform.Rotate(Vector3.down* 1 * speedR);
+		}
+		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.down* -1  * speedR);
+		}
+
+	}
+}
