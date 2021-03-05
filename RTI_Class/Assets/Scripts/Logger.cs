563a092edee241f65354b70cfc5bd5edf762915e
Liam McFadden <liam.e.mcfadden@gmail.com>
2021-03-05 16:34:12 +0100

    Link logger and state in editor, remove unneeded var

--- RTI_Class/Assets/Scripts/Logger.cs
+++ RTI_Class/Assets/Scripts/Logger.cs
@@ -8,7 +8,6 @@ public class Logger : MonoBehaviour
 	public string position_file_pre; // file will be named 'position_file_pre_<name>_<age>_<gender>_<group>.csv'
 	public string heading_file_pre; // file will be named 'heading_file_pre_<name>_<age>_<gender>_<group>.csv'
 
-	public string log_file;
     public float log_every; // how long to wait between logging
 
     public GameObject participant;
